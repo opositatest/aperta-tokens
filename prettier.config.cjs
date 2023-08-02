e2a53cc5f541ff8bed4e505c975a36676e0c3164
Diego Lorenzo <dlorenzonespereira@gmail.com>
2023-08-02 14:24:57 +0200

    feature: configure package

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+// https://prettier.io/docs/en/configuration.html
+
+module.exports = {
+  // 120 chars wide lines
+  printWidth: 120,
+  // Always use trailing commas
+  trailingComma: 'all',
+  // Prefer single quotes instead of double quotes
+  singleQuote: true,
+  arrowParens: 'always',
+};
